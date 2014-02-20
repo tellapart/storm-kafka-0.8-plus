3320d01231f9e55a8395c2d0f0010f6cd2d54a11
Michael Chang <mike@tellapart.com>
2014-02-19 20:52:44 -0800

    Use a map from broker hostname -> stunnel port.

--- src/jvm/storm/kafka/DynamicStunnelBrokersReader.java
+++ src/jvm/storm/kafka/DynamicStunnelBrokersReader.java
@@ -7,13 +7,11 @@ import java.util.*;
 
 public class DynamicStunnelBrokersReader extends DynamicBrokersReader {
 
-    private List<Integer> _stunnelPorts;
-    private Map<String, Integer> _hostStunnelPortMap;
+    private Map<String, Number> _hostToStunnelPort;
 
     public DynamicStunnelBrokersReader(Map conf, String zkStr, String zkPath, String topic) {
         super(conf, zkStr, zkPath, topic);
-        _stunnelPorts = (List<Integer>)conf.get("tellapart.storm.stunnel.ports");
-        _hostStunnelPortMap = new HashMap<String, Integer>();
+        _hostToStunnelPort = new HashMap<String, Number>((Map<String, Number>)conf.get("tellapart.storm.stunnel.host_to_stunnel_port"));
     }
 
     @Override
@@ -21,19 +19,7 @@ public class DynamicStunnelBrokersReader extends DynamicBrokersReader {
         try {
             Map<Object, Object> value = (Map<Object, Object>) JSONValue.parse(new String(contents, "UTF-8"));
             String host = (String) value.get("host");
-
-            Integer port;
-            if (_hostStunnelPortMap.containsKey(host)) {
-                port = _hostStunnelPortMap.get(host);
-            } else {
-                // If there are no more stunnel ports to be assigned, throw an error.
-                if (_stunnelPorts.isEmpty()) {
-                    throw new RuntimeException("No remaining stunnel ports to assign to brokers.");
-                }
-                port = _stunnelPorts.get(0);
-                _stunnelPorts.remove(0);
-                _hostStunnelPortMap.put(host, port);
-            }
+            Integer port = (_hostToStunnelPort.get(host)).intValue();
             // Always return 127.0.0.1 here so that stunnel on the client side can be utilized.
             return new Broker("127.0.0.1", port);
         } catch (UnsupportedEncodingException e) {
